66ee2c147357ff07fe7ac4a0678cf66b119632c3
Joseph <josephalba2006@outlook.com>
2023-04-11 12:40:26 +0100

    Hopefully finished :)

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,44 @@ fn main() {
     // Collect all possible words from file
     let data: String = fs::read_to_string("words.txt").expect("Unable to read file");
     let mut words: Vec<&str> = data.split("\n").collect();
-    words.pop();
     for i in words.iter_mut() {
         *i = i.trim();
     }
-    // A bunch of silly setup variables
-    println!("{:?}", words);
-    let word: String = String::from(words[rand::thread_rng().gen_range(0..words.len())]);
-    let mut playing: bool = true;
-    let mut lives: u8 = 8;
-    let mut chars: Vec<String> = Vec::new();
-    let mut input: String;
-    let mut length: usize;
-    // Game loop
-    while playing {
-        length = 0;
-        input = String::new();
-        display_game(&chars, &word, &lives);
-        while length == 0 {
-            length = std::io::stdin().read_line(&mut input).unwrap() - 2;
-            if length == 1 && !chars.contains(&input) {
-                chars.push(input.clone());
-            } else {
-                lives -= 1;
-            }
-            if check_win(&word, &input, &chars) {
-                println!("You win! The word was {}", word);
-                playing = false;
-            } else {
-                lives -= 1;
+    loop {
+        // A bunch of silly setup variables
+        let word: String = String::from(words[rand::thread_rng().gen_range(0..words.len())]);
+        let mut playing: bool = true;
+        let mut lives: u8 = 8;
+        let mut chars: Vec<String> = Vec::new();
+        let mut input: String;
+        let mut length: usize;
+        // Game loop
+        while playing {
+            length = 0;
+            input = String::new();
+            display_game(&chars, &word, &lives);
+            while length == 0 {
+                length = std::io::stdin().read_line(&mut input).unwrap() - 2;
+                // Add letter to guessed letters
+                if length == 1 {
+                    if !word.contains(&input.trim()) {
+                        lives -= 1;
+                    }
+                    if !chars.contains(&input) {
+                        chars.push(input.clone());
+                    }
+                } else if length > 1 {
+                    lives -= 1;
+                }
+                // Check for wins/losses
+                if check_win(&word, &input, &chars) {
+                    println!("You win! The word was {}", word);
+                    playing = false;
+                }
+                if lives == 0 {
+                    println!("You lose! The word was {}", word);
+                    playing = false;
+                }
             }
         }
     }
